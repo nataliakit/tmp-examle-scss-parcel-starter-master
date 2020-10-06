59e53f45ac4fe1057582ecf07d59390f76adb512
Natalia <nataliakit89@gmail.com>
2020-10-06 21:34:32 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+Starter for SCSS and ParcelJs
+
+google: media query css / bem / scss / parcelJs
+
+install package: npm i
+run dev server: npm run start
+
+go to dev server: http://localhost:1234/
---
+++ src/scss/script.js
@@ -0,0 +1,9 @@
+const buttonList = document.querySelectorAll('.item_button');
+
+buttonList.forEach(buttonRef => {
+    buttonRef.addEventListener('click', toggleElement)
+});
+
+function toggleElement(e) {
+    
+}
